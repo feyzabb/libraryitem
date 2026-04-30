a0ea03eed80ba02921d500aa5ad4a82eb391154d
serpilozdemir.3065@gmail.com <serpilozdemir.3065@gmail.com>
2026-04-30 13:04:36 +0300

    ilk commit basarili

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Library_Item.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(string Email, string Password)
+        {
+           
+            return RedirectToAction("Index", "Home");
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(string Email, string Password, string Username)
+        {
+            
+            return RedirectToAction("Index", "Home");
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Basket()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Controllers/ItemController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Library_Item.Controllers
+{
+    public class ItemController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
